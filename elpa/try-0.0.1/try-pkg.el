6324ff657ab316ef045d5420b38d03db7a1daafd
brendasantana04 <dollanganger.8@gmail.com>
2025-01-31 10:58:21 -0300

    first commit

---
+++ elpa/try-0.0.1/try-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "try" "0.0.1"
+  "Try out Emacs packages."
+  '((emacs "24"))
+  :url "http://github.com/larstvei/try"
+  :commit "271b0a362cadf44d0694628b9e213f54516ef913"
+  :revdesc "271b0a362cad"
+  :keywords '("packages")
+  :authors '(("Lars Tveito" . "larstvei@ifi.uio.no"))
+  :maintainers '(("Lars Tveito" . "larstvei@ifi.uio.no")))
